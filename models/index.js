b6a9c7d0705d652d81d964a82696d38aee5f3214
vuan <wowo94440@gmail.com>
2024-12-22 01:16:20 +0800

    修改環境配置

--- models/index.js
+++ models/index.js
@@ -5,24 +5,19 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
 const config = require("../config/config.json");
 const db = {};
 
 let sequelize;
-if (config.development.use_env_variable) {
-  sequelize = new Sequelize(
-    process.env[config.development.use_env_variable],
-    config
-  );
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+const env = process.env.NODE_ENV || "development";
+
+// 直接使用config.json配置資料庫連線
+sequelize = new Sequelize(
+  config[env].database,
+  config[env].username,
+  config[env].password,
+  config[env] // 其他設定
+);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
