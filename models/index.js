155fe50d15e0da69666c122b8b1c88585a18ed20
vuan <wowo94440@gmail.com>
2024-12-22 01:07:48 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -11,7 +11,10 @@ const db = {};
 
 let sequelize;
 if (config.development.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(
+    process.env[config.development.use_env_variable],
+    config
+  );
 } else {
   sequelize = new Sequelize(
     config.database,
